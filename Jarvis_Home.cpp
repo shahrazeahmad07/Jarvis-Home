22c58a55851f8b38856e854b712caa2a562689cf
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2020-08-25 12:18:26 +0500

    Update Jarvis_Home.cpp

--- Jarvis_Home.cpp
+++ Jarvis_Home.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <conio.h>
 #include <limits>
+#include <iomanip>
 using namespace std;
 main()
 {
-    
+    ///////////// Opening statement //////////////
+    cout << setw(50) << "Jarvis Home" << endl;
+    cout << setw(53) << "A Smart Home Agent" << endl;
+
     getch();
     return 0;
 }

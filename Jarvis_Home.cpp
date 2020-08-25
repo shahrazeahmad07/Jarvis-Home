1482adace11acb0cf4344c144eb4c1ba64e07243
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2020-08-25 12:31:41 +0500

    Update Jarvis_Home.cpp

--- Jarvis_Home.cpp
+++ Jarvis_Home.cpp
@@ -9,6 +9,14 @@ main()
     cout << setw(50) << "Jarvis Home" << endl;
     cout << setw(53) << "A Smart Home Agent" << endl;
 
+    ///////////// House Description //////////////
+    cout << "(This house has 1 guest room, 1 master room, a kitchen, a drawing room and a coridor.\
+ There is only one door through which you can enter or leave the house. The Master room has some\
+ more security features which only master can access. The temperature of the house is fully \
+ maintained by automatic temperature controller. Only Master has a special security key (Master\
+ key) with which he can open any door of the house.)";
+
+
     getch();
     return 0;
 }

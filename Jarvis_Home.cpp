cef392c61d4942c2166f142109ee1e57b505dbd8
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2020-08-27 00:21:15 +0500

    Update Jarvis_Home.cpp

--- Jarvis_Home.cpp
+++ Jarvis_Home.cpp
@@ -12,10 +12,12 @@ main()
     ///////////// House Description //////////////
     cout << "(This house has 1 guest room, 1 master room, a kitchen, a drawing room and a coridor.\
  There is only one door through which you can enter or leave the house. The Master room has some\
- more security features which only master can access. The temperature of the house is fully \
- maintained by automatic temperature controller. Only Master has a special security key (Master\
+ more security features which only master can access. Only Master has a special security key (Master\
  key) with which he can open any door of the house. There is also a safe room in Master room\
- which is accessed with master key only.)";
+ which is accessed by master key only. The temperature of the house is fully maintained by\
+ automatic temperature controller. The house has some more features related to different\
+ appliances of the house. The Jarvis(AI Assistant) take care of different things of house\
+ automatically.)";
 
 
 

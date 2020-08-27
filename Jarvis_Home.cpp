cba8f0125d89202d4c67a2db4b6fabf1a6dc93a5
Shahzaib Ahmad <68849516+shahrazeahmad07@users.noreply.github.com>
2020-08-27 20:07:58 +0500

    Update Jarvis_Home.cpp

--- Jarvis_Home.cpp
+++ Jarvis_Home.cpp
@@ -17,6 +17,8 @@ using namespace std;
     string burglarMasterKeyInput;
     string MasterKey = "74588";
     string mainDoorKey = "12345";
+    bool switchcorridorlight = 0;
+    bool switchmusic = 0;
 
 
 //////////// Beeps ///////////////////
@@ -62,13 +64,34 @@ void mainDoorBurglarProtocol()
     {
         correctBeep();
         cout << "Key Accepted. Burglar's Protocol is disabled." << endl;
-        cout << "(The house is unsealed and the main door opens. Lights of corridor and kitchen\
- is turned on.)" << endl;
+        cout << "(The house is unsealed and the main door opens. Lights of corridor and kitchen are\
+ turned on. Some basic lights of the house are also turned on.)" << endl;
+        switchcorridorlight = 1;
         cout << "Master someone tried to enter your house and I have initiated Burglar's Protocol" << endl;
         cout << "Your house is in safe Hands. You don't need to worry." << endl;
         cout << "I have also called Police at that time. You can now ask the police about the\
  situation." << endl;
         cout << "Get Relax. Everything is safe." << endl;
+        cout << "Do you want me to turn on the music?" << endl;
+    string musicAnswer;
+    musicAnswerAgain:
+    getline(cin, musicAnswer);
+    if (musicAnswer == "Yes" || musicAnswer == "yes" || musicAnswer == "Y" || musicAnswer == "y")
+    {
+        cout << "Playing Music.." << endl;
+        switchmusic = 1;
+        cout << "(music is playing)" << endl;
+    }
+    else if (musicAnswer == "No" || musicAnswer == "no" || musicAnswer == "N" || musicAnswer == "n" ||)
+    {
+        cout << "Okay Sir..!" << endl;
+    }
+    else
+    {
+        cout << "Dont Understand you Sir.\nPlease tell me again: ";
+        goto musicAnswerAgain;
+    }
+
 
     }
 }
@@ -80,12 +103,13 @@ void mainDoorBurglarProtocol()
 
 main()
 {
+    
     ///////////// Opening statement //////////////
     cout << setw(50) << "Jarvis Home" << endl;
     cout << setw(53) << "A Smart Home Agent" << endl;
 
     ///////////// House Description //////////////
-    cout << "(This house has 1 guest room, 1 master room, a kitchen, a drawing room and a coridor.\
+    cout << "(This house has 1 guest room, 1 master room, a kitchen, a drawing room and a corridor.\
  There is only one door through which you can enter or leave the house. The Master room has some\
  more security features which only master can access. Only Master has a special security key (Master\
  key) with which he can open any door of the house. There is also a safe room in Master room\
@@ -105,14 +129,14 @@ main()
     if (mainDoorKeyInput == MasterKey)
     {
         cout << "Access Granted." << endl;
-        cout << "Welcome Master..!" << endl;
         correctBeep();
+        cout << "Welcome Master..!" << endl;
     }
     else if (mainDoorKeyInput == mainDoorKey)
     {
         cout << "Access Granted." << endl;
-        cout << "Welcome Sir..!" << endl;
         correctBeep();
+        cout << "Welcome Sir..!" << endl;
     }
     else
     {
@@ -129,8 +153,30 @@ main()
     }
 
     ///////////// Corridor /////////////
+    cout << "(Lights of corridor and Kitchen are turned on. Some basic lights of the house\
+ are also turned on)";
+    cout << "Do you want me to turn on the music?" << endl;
+    string musicAnswer;
+    musicAnswerAgain:
+    getline(cin, musicAnswer);
+    if (musicAnswer == "Yes" || musicAnswer == "yes" || musicAnswer == "Y" || musicAnswer == "y")
+    {
+        cout << "Playing Music.." << endl;
+        switchmusic = 1;
+        cout << "(music is playing)" << endl;
+    }
+    else if (musicAnswer == "No" || musicAnswer == "no" || musicAnswer == "N" || musicAnswer == "n" ||)
+    {
+        cout << "Okay Sir..!" << endl;
+    }
+    else
+    {
+        cout << "Dont Understand you Sir.\nPlease tell me again: ";
+        goto musicAnswerAgain;
+    }
+    
     Corridor:
-    cout << "corridor";
+    
     
     
     
